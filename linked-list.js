a607afe492022a782c6ffb2179c3475e05599188
Adrik <adrikshadow11@live.com>
2022-06-21 19:12:38 -0400

    finished class LinkedList

---
+++ linked-list.js
@@ -0,0 +1,38 @@
+class LinkedListNode {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+
+  }
+}
+
+class LinkedList {
+  constructor() {
+    // Your code here
+    this.head = null
+    this.length = 0
+  }
+
+  addToHead(val) {
+    // Your code here
+    
+  }
+
+  addToTail(val) {
+    // Your code here
+  }
+
+  // You can use this function to help debug
+  print() {
+    let current = this.head;
+
+    while (current) {
+      process.stdout.write(`${current.value} -> `);
+      current = current.next;
+    }
+
+    console.log("NULL");
+  }
+}
+
+module.exports = LinkedList;
